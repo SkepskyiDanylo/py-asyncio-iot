2ea702be29a1c5748f86599c6dd7d8f8b5bc35ef
Danylo Skepskyi <kol230305@gmail.com>
2025-12-23 19:39:14 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,27 +1,36 @@
 import time
-
+import asyncio
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
     # create and register a few devices
+    # It cannot be async (init)
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+
+    tasks = [
+        asyncio.create_task(service.register_device(hue_light)),
+        asyncio.create_task(service.register_device(speaker)),
+        asyncio.create_task(service.register_device(toilet)),
+    ]
+
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(*tasks)
 
     # create a few programs
     wake_up_program = [
         Message(hue_light_id, MessageType.SWITCH_ON),
         Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(
+            speaker_id,
+            MessageType.PLAY_SONG,
+            "Rick Astley - Never Gonna Give You Up"),
     ]
 
     sleep_program = [
@@ -32,13 +41,13 @@ def main() -> None:
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await service.run_program(wake_up_program)
+    await service.run_program(sleep_program)
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
